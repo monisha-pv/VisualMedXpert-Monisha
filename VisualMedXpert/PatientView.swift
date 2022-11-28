87389ceb8e88b6c1b8bd126599fd6f47efeedf20
Monisha Vadivelu <72807111+monisha-pv@users.noreply.github.com>
2022-11-28 02:56:17 +0000

    VisualMedXpert iOS application - CRUD functions are working

---
+++ VisualMedXpert/PatientView.swift
@@ -0,0 +1,23 @@
+//
+//  PatientView.swift
+//  VisualMedXpert
+//
+//  Created by Monisha Vadivelu on 12/11/2022.
+//
+
+import SwiftUI
+
+struct PatientView: View {
+    var body: some View {
+        Text("Hello, Patient")
+    }
+}
+
+
+
+
+struct PatientView_Previews: PreviewProvider {
+    static var previews: some View {
+        PatientView()
+    }
+}
---
+++ VisualMedXpert/VisualMedXpertApp.swift
@@ -0,0 +1,29 @@
+//
+//  VisualMedXpertApp.swift
+//  VisualMedXpert
+//
+//  Created by Monisha Vadivelu on 12/11/2022.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct VisualMedXpertApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
