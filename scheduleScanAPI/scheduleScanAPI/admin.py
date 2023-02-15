1829beb9938564c4b0caaf59a47de00e3d7d7ca5
Monisha Vadivelu <72807111+monisha-pv@users.noreply.github.com>
2023-02-15 01:30:58 +0000

    delete schedule scan api - runserver issues

--- scheduleScanAPI/scheduleScanAPI/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import scheduleScan
-
-admin.site.register(scheduleScan)
--- scheduleScanAPI/scheduleScanAPI/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import scheduleScan
-
-class scheduleScanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = scheduleScan
-        fields = ['id', 'name', 'gender', 'condition', 'scanType', 'centre', 'date', 'time']
-
-
